ba78fea194ba0891dd20503c36bc0b92f92296f7
Naloj <jolan.owang@gmail.com>
2026-02-15 14:54:31 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+# Suppress TensorFlow oneDNN and other INFO messages before any TF import
+import os
+os.environ.setdefault('TF_CPP_MIN_LOG_LEVEL', '2')  # 0=all, 1=no INFO, 2=no INFO/WARNING
+
 from app import app, db
 
 if __name__ == '__main__':
